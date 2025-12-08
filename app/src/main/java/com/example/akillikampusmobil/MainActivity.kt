31a108e2ded0cc8b306b8ef3572fc7367a362e6a
Tugce Yagcioglu <tugce.yagcioglu3@gmail.com>
2025-12-08 21:33:12 +0300

    Admin ve User rolleri verildi. Register Activity kısmı oluşturuldu.

--- app/src/main/java/com/example/akillikampusmobil/MainActivity.kt
+++ app/src/main/java/com/example/akillikampusmobil/MainActivity.kt
@@ -15,26 +15,65 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-     //XML bileşenlerini bulup kullanmak için yazıyoruz
+        //XML bileşenlerini bulup kullanmak için yazıyoruz
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val etPassword = findViewById<EditText>(R.id.etPassword)
         val btnLogin = findViewById<Button>(R.id.btnLogin)
 
         btnLogin.setOnClickListener {
-            val email =etEmail.text.toString()
+            val email = etEmail.text.toString()
             val password = etPassword.text.toString()
 
-            if(email.isBlank() || password.isBlank() ){
-                Toast.makeText(this, "Lutfen alanlari doldurun", Toast.LENGTH_SHORT).show()}
-            else {
-                Toast.makeText(this, "Giris Basarili!", Toast.LENGTH_SHORT).show()}
-
+            if (email.isBlank() || password.isBlank()) {
+                Toast.makeText(this, "Lutfen alanlari doldurun", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener // Hata varsa kodu burada KES!
             }
 
+            // 3. ROL MANTIĞI
+            // Gerçek hayatta burayı veritabanından soracağız.
+            // Şimdilik sadece "özel admin hesabı" Admin olsun, geri kalan herkes User olsun.
+            val role: String
+
+            if (email == "admin@akillikampus.edu.tr" && password == "123456") {
+                // Özel yönetici girişi
+                role = "Admin"
+            } else {
+                // Diğer herkes (Kayıt olanlar varsayılan User'dır )
+                role = "User"
+            }
+
+            // 4. Sonuç ve Yönlendirme
+            // İleride buraya "if (role == "Admin") AdminActivity'yi aç" diyeceğiz.
+
+
+            Toast.makeText(this, "Giriş Başarılı! Rolünüz: $role", Toast.LENGTH_LONG).show()
+        }
+        // 1. Yazıları Tanımla
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
+        val tvForgotPassword = findViewById<TextView>(R.id.tvForgotPassword)
+
+        // 2. Kayıt Ol'a Tıklanınca (PDF Madde: Kayıt Ol Ekranına Geçiş)
+        tvRegister.setOnClickListener {
+            // RegisterActivity'yi (Kayıt Ekranını) başlat
+            val intent = android.content.Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
 
+        // 3. Şifremi Unuttum'a Tıklanınca (PDF Madde: Bilgilendirme Simülasyonu)
+        tvForgotPassword.setOnClickListener {
+            Toast.makeText(this, "Şifre sıfırlama bağlantısı e-postanıza gönderildi.", Toast.LENGTH_LONG).show()
+        }
 
     }
+}
+
+
+
+
+
+
+
+
 
 
 
